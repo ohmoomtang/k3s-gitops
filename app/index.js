2953b2d3918f4f6248f33513b86056564a7dcb6a
Napawan Srisuksawad <1062485+ohmoomtang@users.noreply.github.com>
2026-05-09 01:45:57 +1000

    Again for canary

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.end(`
     <html>
       <body style="font-family: monospace; padding: 40px;">
-        <h2 style="color:red">🚀 Hello from K3s!</h2>
+        <h2 style="color:red">🚀 Hello from K3s! v2</h2>
         <p><b>Pod:</b> ${os.hostname()}</p>
         <p><b>Node IP:</b> ${
           Object.values(os.networkInterfaces())
