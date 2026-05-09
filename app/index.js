77c833cb053bbc1a06c4066d25accab9a3ffc2f7
Napawan Srisuksawad <1062485+ohmoomtang@users.noreply.github.com>
2026-05-10 00:13:18 +1000

    Add log everytime request made

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const os = require('os');
 
 const server = http.createServer((req, res) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(`
     <html>
